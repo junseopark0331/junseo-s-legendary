921c87572863a7ea361f0924c2429514cbb48f71
junseopark <junseopark0331@icloud.com>
2022-05-23 23:29:27 +0900

    UI KIT으로 앱을 만드는 과정에 대하여 배웠다 inset 과 offset에 대해서도 배웠는데 내가 느끼기엔 inset이 조금 더 융통성 있는것 같은 부분이다. 직접 색깔을 설정해서 만들어보니 신기했다 ㅎㅎ..

--- junseo's legendary/ViewController.swift
+++ junseo's legendary/ViewController.swift
@@ -6,14 +6,30 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    lazy var sampleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .yellow
+        return view
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        self.view.addSubview(sampleView)
+        sampleView.snp.makeConstraints{(make)in //.snp.makeConstraints = 위치조정
+            make.width.equalTo(self.view) // View와 같은 크기의 width를 가지겠다
+            make.height.equalTo(70) // 70크기의 height를 가지겠다
+            make.top.equalTo(self.view.safeAreaLayoutGuide)
+            //safeAreaLayoutGuide를 사용해주면 위에 있는 시간이나 배터리가 잘려보이지 않는다
+            // 70크기의 height를 가졌지만 bottom으로 30 내려왔으므로 실질적으로 보이는것은 40이다.
+        }
         // Do any additional setup after loading the view.
     }
-
-
 }
 
