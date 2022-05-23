d97843db807da3abbf6808587cb154e10383ba07
junseopark <junseopark0331@icloud.com>
2022-05-23 22:47:01 +0900

    Initial Commit

---
+++ junseo's legendary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  junseo's legendary
+//
+//  Created by 박준서 on 2022/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
